ccafa770c829b77e691849c8d0555a497b7389bc
alexbol93 <49373895+alexbol93@users.noreply.github.com>
2020-07-06 11:46:30 +0300

    add some tests

--- tests/test_base_library.py
+++ tests/test_base_library.py
@@ -1,3 +1,5 @@
+import string
+
 def test_len_of_strings(random_string):
     assert len(random_string) == 10
 
@@ -6,8 +8,20 @@ def test_reversing(random_string):
     rev_str1 = "drowasss"
     assert random_string[::-1] == rev_str1
 
+
 def test_zero_devide(random_number):
     try:
         random_number/0
     except ZeroDivisionError as zd:
         assert type(zd).__name__ == 'ZeroDivisionError'
+
+
+class TestLists:
+
+    def test_search_in_list(self):
+        l = [l for l in 'word' if l in string.ascii_lowercase]
+        assert 'w' in l
+        assert 'o' in l
+
+    def tes_revercing(self):
+        pass
